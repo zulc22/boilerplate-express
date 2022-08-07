9b0653a561675a6c0d75aeca059068c31ed272dc
zulc22 <zulc22@users.noreply.github.com>
2022-08-07 04:03:27 -0700

    generic commit message

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
+require('dotenv').config()
+
 let express = require('express');
 let app = express();
 
+app.use((req,res,next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
 
+app.get("/json", (req, res) => {
+    res.json({
+        message:
+            process.env.MESSAGE_STYLE==="uppercase" ?
+            "HELLO JSON" : "Hello json"
+    })
+})
 
+app.use("/public", express.static(__dirname + "/public"))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
